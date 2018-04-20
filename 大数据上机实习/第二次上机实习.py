5eebcbbc52aa72d9577b15af39302499c94cfb94
Lei Yang <2844371986@qq.com>
2018-04-20 11:08:34 +0800

    Update
    cassandra

--- 大数据上机实习/第二次上机实习.py
+++ 大数据上机实习/第二次上机实习.py
@@ -2,6 +2,8 @@
 from pymongo import MongoClient
 from collections import defaultdict
 import redis
+from cassandra.cluster import Cluster
+
 
 def test_dict():
     l = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
@@ -78,10 +80,11 @@ def redis_store(file, r, sep):
             r.sadd(o, '{} {}'.format(s,p))
             r.sadd(p, s)
             r.hsetnx(o+'hash', s, 0) # key-hash表，键值key不能重复
-            r.hincrby(o+'hash', s, 1)
+            r.hincrby(o+'hash', s, 1) # 递增计数
 
 
 def redis_main(file, sep, arg_list, store=False):
+    # 需要注意键值不能重复
     # 连接redis
     r = redis.Redis(host='127.0.0.1', port=6379, db=0)
     if store:
@@ -115,18 +118,72 @@ def redis_main(file, sep, arg_list, store=False):
     print(rs[b'Liam_Tyson'])
 
 
-def cassandra_main():
-    pass
+def cassandra_store(file, session, sep):
+    key_string = '(id,S1,P1,O1)'
+    id = 0
+    with open(file) as f:
+        for line in f:
+            line = line.strip('\n')
+            id += 1
+            s, p, o = line.split(' ')
+            values_string = "({},'{}','{}','{}')".format(id,s,p,o)
+            insert = "insert into bigdata.lord" + key_string + "values" + values_string
+            session.execute(insert)
+
+
+def cassandra_main(file, sep, arg_list, store=False):
+    # 需要注意查询插入语句中字符串引号的使用
+    # allow data filter
+    # 键不区分大小写
+    cluster = Cluster(['127.0.0.1'])
+    session = cluster.connect()
+    session.execute(
+        "create keyspace if not exists bigdata with replication={'class':'SimpleStrategy', 'replication_factor': 1}")
+    session.execute('use bigdata')
+
+    # session.execute('create table lord (id int primary key, S1 text, P1 text, O1 text)')
+
+    if store:
+        cassandra_store(file, session, sep)
+
+    # 有S，查所有P，O
+    rs = session.execute("select * from bigdata.lord where S1='{}' allow filtering".format(arg_list[0]))
+    print('查询1：')
+    for i in rs:
+        print('{},{}'.format(i.s1, i.o1))
+
+    # 有O，查S，P
+    rs = session.execute("select * from bigdata.lord where O1='{}' allow filtering".format(arg_list[1]))
+    print('查询2：')
+    for i in rs:
+        print('{},{}'.format(i.s1, i.p1))
+
+    # 给出p1，p2，查同时拥有它们的S
+    rs1 = session.execute("select * from bigdata.lord where P1='{}' allow filtering".format(arg_list[2]))
+    rs2 = session.execute("select * from bigdata.lord where P1='{}' allow filtering".format(arg_list[3]))
+    result_set = set(rs1).intersection(set(rs2))
+    print('查询3：')
+    for i in result_set:
+        print(i.s1)
+
+    # 给定O，查拥有O最多的S
+    rs = session.execute("select * from bigdata.lord where O1='{}' allow filtering".format(arg_list[4]))
+    d = defaultdict(int)
+    for i in rs:
+        d[i.s1] += 1
+    result = sorted(d.keys(), key=lambda obj:d[obj], reverse=True)[0] if len(d)>0 else None
+    print('查拥有 {} 最多的S：{},拥有:{}'.format(arg_list[4], result, d.get(result, None)))
 
 
 def main():
     S1 = 'Liam_Tyson'
     O1 = 'France'
-    P1, P2 = 'isCitizenOf', 'isLeaderOf'
-    O2 = 'England'
+    P1, P2 = 'isCitizenOf', 'owns'
+    O2 = 'Cuba'
     arg_list = [S1, O1, P1, P2, O2]
     # mdb_main('yagoThreeSimplified.txt', ' ', arg_list)
-    redis_main('test.txt', ' ', arg_list, True)
+    # redis_main('test.txt', ' ', arg_list, True)
+    cassandra_main('test.txt', ' ', arg_list)
 
 
 if __name__ == '__main__':
