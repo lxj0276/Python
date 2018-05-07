0ad16222ac293ba019819cbd2ed679230a5e35c1
Lei Yang <2844371986@qq.com>
2018-05-07 15:14:25 +0800

    Update

--- PyQuant/UtilityCheck.py
+++ PyQuant/UtilityCheck.py
@@ -1,21 +1,87 @@
 import tushare as ts
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-def ts_check():
+def pd_check():
     result = ts.get_hist_data('600000')
-    # print(result)
-    # print(type(result))
-    print(result.columns)
-    result['close'].plot( )
+    t = type(result)
+    columns = result.columns
 
-    plt.show()
-    print(result.index)
+    # 生成一列年份数据
+    result['year'] = result.index
+    result['year'] = result['year'].apply(lambda obj:obj[:4])  # 应用 apply 函数
+    result['date'] = result.index
+    data = result[result['date'].apply(lambda obj:obj[:4]=='2018')]  # 索引筛选
+
+    # 排序和排名
+    result.sort_index(by='open')  # 按照某个索引排序
+    result['open'].rank(method='first')  # 用来破坏平级关系，否则用 平均排名 表示
+
+    # 描述性统计
+    result.mean() # 默认是列方向
+    result['open'].idxmax()  # 查看最高值的日期、索引
+    result['open'].idxmin()  # 查看最低值的日期、索引
+    result['close'].quantile(0.5)  # 分位数
+    result['p_change'].cumprod()  # 累计积
+    result['close'].diff()  # 一阶查分
+    result['close'].pct_change()  # 百分比变化
+    result['close'].corr(result['open'])  # 相关系数, pd.DataFrame.corrwith()
+    result['close'].cov(result['open'])  # 协方差
+
+    # 处理缺失数据
+    result.dropna()
+    result = result[result.notnull()]
+    result.fillna(0, method='ffill', inplace=True)  # inplace 就地修改
+
+    # 读取
+    pd.read_csv()
+    pd.read_table()
+    '''
+    sep/deliminator:regex
+    encoding
+    '''
+
+    # 合并数据集
+    pd.merge(result, data, on='date', how='outer')
+    '''
+    right_index/left_index : True/False
+    on/left_on/right_on : [...]
+    how : outer, inner, left, right
+    '''
+    result.join(data)  # 默认取索引的版本
+    pd.concat([data, result], axis=1)  # 轴向连接, axis=1表示行方向上进行
+    '''
+    axis : 0/1 
+    join_axes : ['a']
+    '''
+    np.where(result.isnull(), result, data)
+    result.combine_first(data)
+    '''
+    others:
+    stack/unstack
+    drop_duplicates
+    duplicates : True/False Series
+    '''
+    result['close'].map(str.lower)  # 类似于apply
+    result['close'].replace()
+
+    # 字符串(支持正则)
+    data['data'].str.contains('mail')
+    pattern = '.*'
+    data['data'].str.findall(pattern)
+    matches = data['data'].str.match(pattern)  # 匹配
+    matches.str.get(1)
+    matches = matches.str[0]  # 取出匹配结果
+
+
+def ts_check():
+    pass
 
 
 def main():
-    ts_check()
+    pd_check()
 
 
 if __name__ == '__main__':
