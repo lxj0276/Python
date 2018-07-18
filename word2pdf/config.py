ccacb6c5eb55dfcdab84b86628946be305139fef
Lei Yang <2844371986@qq.com>
2018-07-18 15:21:00 +0800

    Update

---
+++ word2pdf/config.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+
+REPORT_DOC_PATH = r"C:\pythonApp\word2pdf\word"
+REPORT_PDF_PATH = r"C:\pythonApp\word2pdf\pdf"
--- word2pdf/word2pdf.py
+++ word2pdf/word2pdf.py
@@ -1,28 +1,35 @@
 # -*- encoding: utf-8 -*-
 import os
 from win32com import client
+from config import REPORT_DOC_PATH, REPORT_PDF_PATH
 
 
-def doc2pdf(doc_name, pdf_name):
-    """
-    :word文件转pdf
-    :param doc_name word文件名称
-    :param pdf_name 转换后pdf文件名称
-    """
-    try:
-        word = client.DispatchEx("Word.Application")
-        if os.path.exists(pdf_name):
-            os.remove(pdf_name)
-        worddoc = word.Documents.Open(doc_name, ReadOnly=1)
-        worddoc.SaveAs(pdf_name, FileFormat=17)
-        worddoc.Close()
-        return pdf_name
-    except:
-        return 1
+def word2pdf(filename):
+
+    pdf_name = filename.split('.doc')[0] + '.pdf'
+
+    if not os.path.isabs(filename):
+        os.chdir(REPORT_DOC_PATH)
+        filename = os.path.abspath(filename)
+
+    if not os.path.isabs(pdf_name):
+        os.chdir(REPORT_PDF_PATH)
+        pdf_name = os.path.abspath(pdf_name)
+
+    print(filename, pdf_name)
+    # 2007 需要用 gencache
+    word = client.DispatchEx("Word.Application")
+    if os.path.exists(pdf_name):
+        os.remove(pdf_name)
+    doc = word.Documents.Open(filename, ReadOnly=1)
+    doc.SaveAs(pdf_name, FileFormat=17)
+    doc.Close()
+    return pdf_name
 
 
 if __name__ == '__main__':
     # 只能是绝对路径
-    doc_name = r"C:\pythonApp\word2pdf\1.docx"
-    ftp_name = r"C:\pythonApp\word2pdf\1.pdf"
-    doc2pdf(doc_name, ftp_name)
+
+    for _, _, filenames in os.walk(REPORT_DOC_PATH):
+        for file in filenames:
+            word2pdf(file)
