f4a4ce632452303015756412293ae5bb0f36a5a5
Lei Yang <2844371986@qq.com>
2018-08-17 14:31:27 +0800

    Update

--- LargeAssets/Back.py
+++ LargeAssets/Back.py
@@ -7,37 +7,47 @@ class Context:
     def __init__(self, context=None):
         if context is None:
             self.BktestParam = {
-                # 'init_period': ['2005-03-01', '2018-01-31'],
-                'commission_rate': 0,       # 手续费
                 'signal': None,             # 交易信号函数
+                'commission_rate': 0,       # 手续费
                 'refresh_dates': None,      # 调仓日期序列
-                'signal_params': {}         # 交易信号函数需要的额外参数
             }
 
             self.GlobalParam = {
-                # 'data': None,  # 数据
+                'base_nav': None,           # 基准的净值曲线
                 'asset_pool': None,         # 资产池
                 'daily_close': None,        # 资产每日的收盘价
-                'daily_dates': None,        # 交易日期序列
-                'base_nav': None            # 基准的净值曲线
+                'daily_dates': None         # 交易日期序列
             }
 
             self.BktestResult = {
                 'w': None,                  # 每个调仓日期的权重
+                'df': None,                 # 回测结果
                 'nav': None,                # 回测的净值曲线
-                'nav_perf': None,           # 回测表现
-                'df': None                  # 回测结果
+                'nav_perf': None            # 回测表现
             }
+
+            self.Add = {
+                'signal_params': {}         # 交易信号函数需要的额外参数
+            }                               # 用户自定义
         else:
             self.GlobalParam = context.GlobalParam
             self.BktestParam = context.BktestParam
             self.BktestResult = context.BktestResult
 
     def cal_long_weight(self):
-        refresh_dates = []
+
+        assert self.BktestParam['signal'] is not None, '请定义调仓函数：BktestParam["signal"]'
+        assert self.GlobalParam['daily_close'] is not None, '需要资产每日的收盘价才能回测！GlobalParam["daily_close"]'
+
+        self.GlobalParam['daily_close'] = self.GlobalParam['daily_close'].fillna(method='bfill')
+        self.GlobalParam['daily_dates'] = self.GlobalParam['daily_close'].index.tolist()
+        self.GlobalParam['asset_pool'] = self.GlobalParam['daily_close'].columns
         self.BktestResult['w'] = pd.DataFrame(columns=self.GlobalParam['asset_pool'])
+
+        # 在每个交易日讨论是否调仓
+        refresh_dates = []
         for date in self.GlobalParam['daily_dates']:
-            sig, weight = self.BktestParam['signal'](self, date, **self.BktestParam['signal_params'])
+            sig, weight = self.BktestParam['signal'](self, date, **self.Add['signal_params'])
             if sig:
                 refresh_dates.append(date)
                 self.BktestResult['w'].loc[date, :] = weight
@@ -47,11 +57,14 @@ class Context:
         return self.BktestResult['w']
 
     def cal_nav(self):
-        close = self.GlobalParam['daily_close']
+
+        assert self.BktestResult['w'] is not None, "需要先计算每次调仓的权重！cal_long_weight()"
+
         w = self.BktestResult['w']
-        commission_rate = self.BktestParam['commission_rate']
+        close = self.GlobalParam['daily_close']
         daily_dates = self.GlobalParam['daily_dates']
         refresh_dates = self.BktestParam['refresh_dates']
+        commission_rate = self.BktestParam['commission_rate']
 
         # 建仓
         nav = pd.Series(index=daily_dates)
